d3aeaee474807f4a48fb4827826eb38c7c853630
Chang Jian Rong <cjr030918d@gmail.com>
2023-01-20 22:06:59 +0800

    Add the four basic operations into the calculator that mimicks lazy evaluation
    When calling one of the four basic operations, they return an expression in the form of a string. The expression is only evaluated when evaluate() is called

--- calculator.py
+++ calculator.py
@@ -1,9 +1,45 @@
 import math
-
+from typing import Union
 class Calculator:
     
     def __init__(self) -> None:
-        pass
+        self.answer: int = 0
+        self.expression: str = "0"
+
+    
+    def __number_checker(self, number):
+        assert type(number) in (int, float)
+
+
+    def add(self, number: Union[int, float]):
+        self.__number_checker(number)
+        self.expression += f" + {number}"
+        return self
+
+    
+    def subtract(self, number: Union[int, float]):
+        self.__number_checker(number)
+        self.expression += f" - {number}"
+        return self
+
+
+    def multiply(self, number: Union[int, float]):
+        self.__number_checker(number)
+        self.expression += f" * {number}"
+        return self
+    
+
+    def divide(self, number: Union[int, float]):
+        self.__number_checker(number)
+        self.expression += f" / {number}"
+        return self
+
+
+    def evaluate(self):
+        self.answer = eval(self.expression, {"__builtins__": {}}, {})
+        self.expression = "0"
+        return self.answer
+
 
 
     def raw_evaluate(self, expression: str):
