cbaba9f579ff46c589570a2ba7b9d9a5ae94b9a2
Chang Jian Rong <cjr030918d@gmail.com>
2023-01-22 18:00:05 +0800

    Change implementation of the how the Calculator class deals with expressions
    This is a breaking change. Calculator acts now as an interface to create expressions. Users use create_expression() to create a CalculatorExpression object to perform arithmetic operations instead of using the Calculator class

--- calculator.py
+++ calculator.py
@@ -1,54 +1,53 @@
 import math
 from typing import Union, Literal
 
-class Calculator:
 
-    class __ExpressionStatement:
-        def __init__(self) -> None:
-            self.expression: str = ""
-
-
-        def number_checker(self, number):
-            assert type(number) in (int, float)
-
-
-        def append_expression(self, number: Union[int, str], operator: Literal["+", "-", "*", "/"]):
-            self.number_checker(number)
-            self.expression += f"{operator}{number}"
-
-        def evaluate(self):
-            result = eval(self.expression, {"__builtins__": {}}, {})
-            self.expression = "0"
-            return result
+class CalculatorExpression:
     
     def __init__(self) -> None:
-        self.answer: int = 0
-        self.__expression = self.__ExpressionStatement()
+        self.__expression: str = ""
+        self.answer: Union[int, float, None] = None
+    
+    
+    def number_checker(self, value):
+        assert type(value) in (int, float, CalculatorExpression)
 
 
-    def add(self, number: Union[int, float]):
-        self.__expression.append_expression(number, "+")
+    def append_expression(self, operator: Literal["+", "-", "*", "/"], value: Union[int, str]):
+        self.number_checker(value)
+        self.__expression += f"{operator}{value}"
+
+
+    def add(self, value: Union[int, float]):
+        self.append_expression(operator="+", value=value)
         return self
     
-    def subtract(self, number: Union[int, float]):
-        self.__expression.append_expression(number, "-")
+    def subtract(self, value: Union[int, float]):
+        self.append_expression(operator="-", value=value)
         return self
 
-    def multiply(self, number: Union[int, float]):
-        self.__expression.append_expression(number, "*")
+    def multiply(self, value: Union[int, float]):
+        self.append_expression(operator="*", value=value)
         return self
 
-    def divide(self, number: Union[int, float]):
-        self.__expression.append_expression(number, "/")
+    def divide(self, value: Union[int, float]):
+        self.append_expression(operator="/", value=value)
+        return self
+
+
+    def clear_expression(self):
+        self.__expression = ""
         return self
 
 
     def evaluate(self):
-        self.answer = self.__expression.evaluate()
-        return self.answer
-
-    def raw_evaluate(self, expression: str):
-        code = compile(expression, "<string>", "eval")
+        code = compile(self.__expression, "<string>", "eval")
         if code.co_names:
             raise NameError("cannot use variable names in the expression")
-        return eval(code, {"__builtins__": {}}, {})
+        answer = eval(code, {"__builtins__": {}}, {})
+        return answer
+
+class Calculator:
+
+    def create__expression(self):
+         return CalculatorExpression()
